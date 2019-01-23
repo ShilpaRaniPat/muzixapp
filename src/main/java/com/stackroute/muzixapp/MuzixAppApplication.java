1796c5a15a89969988f41121b5d38a3805e81b82
shilpa.rani <shilpa.rani@boeing.com>
2019-01-23 14:03:45 +0530

    initial commit : first commit on environment

--- src/main/java/com/stackroute/muzixapp/MuzixAppApplication.java
+++ src/main/java/com/stackroute/muzixapp/MuzixAppApplication.java
@@ -3,18 +3,30 @@ package com.stackroute.muzixapp;
 import com.stackroute.muzixapp.domain.Muzix;
 import com.stackroute.muzixapp.repository.MuzixRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
-
+@PropertySource("classpath:application.properties")
 @Component
 @SpringBootApplication
 
 public class MuzixAppApplication implements ApplicationListener<ContextRefreshedEvent>, CommandLineRunner {
+
+
+	@Value("${trackId}")
+	private int trackId;
+	@Value("${trackName}")
+	private int trackName;
+	@Value("${trackComment}")
+	private int trackComment;
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(MuzixAppApplication.class, args);
@@ -22,14 +34,18 @@ public class MuzixAppApplication implements ApplicationListener<ContextRefreshed
 	@Autowired
    MuzixRepository muzixRepository;
 
-	@Override
-	public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
-		muzixRepository.save(new Muzix(1,"Shilpa","error"));
-	}
 
 	@Override
 	public void run(String...args) {
 		muzixRepository.save(new Muzix(2,"Sajal","highfive"));
+	}
+	@Autowired
+	Environment ev;
+
+	@Override
+	public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+		muzixRepository.save(new Muzix(Integer.parseInt(ev.getProperty("trackId")),ev.getProperty("trackName"),ev.getProperty("trackComment")));
+
 	}
 
 
